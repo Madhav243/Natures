4171e0af5bbf8533a41c93b4a66a3b60403e9877
Madhav243 <madhav.verma108@gmail.com>
2021-10-11 13:21:09 +0530

    Improved Error Handling

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const dotenv = require('dotenv');
 
 const mongoose = require('mongoose');
 const app = require('./app');
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('PASSWORD',process.env.DATABASE_PASSWORD);
@@ -15,6 +20,13 @@ mongoose.connect(DB,{
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);// 0 for success and 1 for uncaught exception
+  });
 });
